a3373fb72520aaf183d17a17325759c63c15fbd1
jys33 <47339183+jys33@users.noreply.github.com>
2019-12-23 16:06:14 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE provincia (
 CREATE TABLE localidad (
   id_localidad INT UNSIGNED NOT NULL,
   nombre VARCHAR (50) NOT NULL,
-  cp CHAR(8) NOT NULL,
+  cp SMALLINT NOT NULL,
   id_provincia TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (id_localidad),
   FOREIGN KEY (id_provincia) REFERENCES provincia (id_provincia)
@@ -28,100 +28,30 @@ CREATE TABLE localidad (
 # Inserción en la tabla provincia
 INSERT INTO provincia 
 VALUES
-  (1, 'BUENOS AIRES', 'AR-B') ;
-
-INSERT INTO provincia 
-VALUES
-  (2, 'CATAMARCA', 'AR-K') ;
-
-INSERT INTO provincia 
-VALUES
-  (3, 'CHACO', 'AR-H') ;
-
-INSERT INTO provincia 
-VALUES
-  (4, 'CHUBUT', 'AR-U') ;
-
-INSERT INTO provincia 
-VALUES
-  (5, 'CIUDAD AUTONOMA DE BUENOS AIRES', 'AR-C') ;
-
-INSERT INTO provincia 
-VALUES
-  (6, 'CORRIENTES', 'AR-W') ;
-
-INSERT INTO provincia 
-VALUES
-  (7, 'CORDOBA', 'AR-X') ;
-
-INSERT INTO provincia 
-VALUES
-  (8, 'ENTRE RIOS', 'AR-E') ;
-
-INSERT INTO provincia 
-VALUES
-  (9, 'FORMOSA', 'AR-P') ;
-
-INSERT INTO provincia 
-VALUES
-  (10, 'JUJUY', 'AR-Y') ;
-
-INSERT INTO provincia 
-VALUES
-  (11, 'LA PAMPA', 'AR-L') ;
-
-INSERT INTO provincia 
-VALUES
-  (12, 'LA RIOJA', 'AR-F') ;
-
-INSERT INTO provincia 
-VALUES
-  (13, 'MENDOZA', 'AR-M') ;
-
-INSERT INTO provincia 
-VALUES
-  (14, 'MISIONES', 'AR-N') ;
-
-INSERT INTO provincia 
-VALUES
-  (15, 'NEUQUEN', 'AR-Q') ;
-
-INSERT INTO provincia 
-VALUES
-  (16, 'RIO NEGRO', 'AR-R') ;
-
-INSERT INTO provincia 
-VALUES
-  (17, 'SALTA', 'AR-A') ;
-
-INSERT INTO provincia 
-VALUES
-  (18, 'SAN JUAN', 'AR-J') ;
-
-INSERT INTO provincia 
-VALUES
-  (19, 'SAN LUIS', 'AR-D') ;
-
-INSERT INTO provincia 
-VALUES
-  (20, 'SANTA CRUZ', 'AR-Z') ;
-
-INSERT INTO provincia 
-VALUES
-  (21, 'SANTA FE', 'AR-S') ;
-
-INSERT INTO provincia 
-VALUES
-  (22, 'SANTIAGO DEL ESTERO', 'AR-G') ;
-
-INSERT INTO provincia 
-VALUES
-  (23, 'TIERRA DEL FUEGO', 'AR-V') ;
-
-INSERT INTO provincia 
-VALUES
+  (1, 'BUENOS AIRES', 'AR-B'),
+  (2, 'CATAMARCA', 'AR-K'),
+  (3, 'CHACO', 'AR-H'),
+  (4, 'CHUBUT', 'AR-U'),
+  (5, 'CIUDAD AUTONOMA DE BUENOS AIRES', 'AR-C'),
+  (6, 'CORRIENTES', 'AR-W'),
+  (7, 'CORDOBA', 'AR-X'),
+  (8, 'ENTRE RIOS', 'AR-E'),
+  (9, 'FORMOSA', 'AR-P'),
+  (10, 'JUJUY', 'AR-Y'),
+  (11, 'LA PAMPA', 'AR-L'),
+  (12, 'LA RIOJA', 'AR-F'),
+  (13, 'MENDOZA', 'AR-M'),
+  (14, 'MISIONES', 'AR-N'),
+  (15, 'NEUQUEN', 'AR-Q'),
+  (16, 'RIO NEGRO', 'AR-R'),
+  (17, 'SALTA', 'AR-A'),
+  (18, 'SAN JUAN', 'AR-J'),
+  (19, 'SAN LUIS', 'AR-D'),
+  (20, 'SANTA CRUZ', 'AR-Z'),
+  (21, 'SANTA FE', 'AR-S'),
+  (22, 'SANTIAGO DEL ESTERO', 'AR-G'),
+  (23, 'TIERRA DEL FUEGO', 'AR-V'),
   (24, 'TUCUMAN', 'AR-T') ;
-
 
 SELECT 
   COUNT(*) 
